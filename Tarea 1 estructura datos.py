d61af17356c23b7dd5ee44fa35c533108166ecea
arojasbjj <a.rojas.m90@gmail.com>
2023-05-21 06:50:36 -0600

    Tarea #1

---
+++ Tarea 1 estructura datos.py
@@ -0,0 +1,11 @@
+#21/5/23
+#Andres Rojas Murillo
+#UH, Estructura de datos
+#Tarea #1
+
+#ARREGLO 1
+arreglo1=[5,7,2,4,6]
+arreglo2=[8,10,1,3,20]
+arreglo1.extend(arreglo2)
+arreglo1.sort(reverse=True)
+print(arreglo1)
